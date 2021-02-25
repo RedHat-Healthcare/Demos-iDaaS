9c0eb14ac21185e386f51805fe00e2c49aa1996f
tarilabs <matteo.mortari@gmail.com>
2021-02-25 14:53:14 +0100

    Step1: avoid file copypaste with no history

--- DREAM/idaas-dream-demo/src/main/java/com/redhat/idaas/dream/demo/DemoMain.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.idaas.dream.demo;
-
-public class DemoMain {
-    public static void main(String[] args) throws Exception {
-        System.out.println("There are several demos available on this project, launching by default CamelEmbeddedRulesMain");
-        DemoCamelEmbeddedRulesMain.main(args);
-    }
-}
